bde474f77722a7ee0a71c9ca36e1fec401668e84
Yassine <qoraicheofficiel@hotmail.com>
2019-03-03 00:26:37 +0100

    update footer signature

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-right mt-4" style="font-size: 14px;">
       <div class="text-muted">
-          <p>© 2019 - <a href="">Qoraiche.me</a> - <a href="#muted">MailEclipse</a> v1.0</p>
+          <p>© 2019 - <a href="https://github.com/Qoraiche/laravel-mail-editor">MailEclipse</a> v1.0</p>
       </div>        
       
  </footer>
