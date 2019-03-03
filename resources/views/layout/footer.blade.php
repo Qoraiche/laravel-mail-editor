254b913f69cbfcc8ef5099a734c51293c4527dd4
Yassine <qoraicheofficiel@hotmail.com>
2019-03-03 03:05:06 +0100

    update footer version

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-right mt-4" style="font-size: 14px;">
       <div class="text-muted">
-          <p>© 2019 - <a href="https://github.com/Qoraiche/laravel-mail-editor" target="_blank">MailEclipse</a> v1.0</p>
+          <p>© 2019 - <a href="https://github.com/Qoraiche/laravel-mail-editor" target="_blank">MailEclipse</a> v1.0.0</p>
       </div>        
       
  </footer>
