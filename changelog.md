1fc567a145971a1d978f190b0cdfa95d4bc0d169
Yassine <qoraicheofficiel@hotmail.com>
2019-03-07 00:10:35 +0100

    update version

--- changelog.md
+++ changelog.md
@@ -8,7 +8,4 @@ All notable changes to `laravel-mail-editor` will be documented in this file.
 - Fix error `Call to a member function sortBy() on null` that happens when mailables directory not created yet.
 - Replace jquery `$.ajax` with `axios` library, also adding notie confirmation for save-template action
 - Fix javascript error `plaintextEditor is not defined` when save mailable view template.
-
-## Version 1.1
-
 - Fix mailable with multiple constructor args type error ([issue](https://github.com/Qoraiche/laravel-mail-editor/issues/1)).
--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-right mt-4" style="font-size: 14px;">
       <div class="text-muted">
-          <p>© 2019 - <a href="https://github.com/Qoraiche/laravel-mail-editor" target="_blank">MailEclipse</a> v1.1</p>
+          <p>© 2019 - <a href="https://github.com/Qoraiche/laravel-mail-editor" target="_blank">MailEclipse</a> v1.0</p>
 			<a class="github-button" href="https://github.com/qoraiche/laravel-mail-editor/issues" data-icon="octicon-issue-opened" aria-label="Issue qoraiche/laravel-mail-editor on GitHub">Issue</a>
 
 			<a class="github-button" href="https://github.com/qoraiche/laravel-mail-editor" data-icon="octicon-star" data-show-count="true" aria-label="Star qoraiche/laravel-mail-editor on GitHub">Star</a>
