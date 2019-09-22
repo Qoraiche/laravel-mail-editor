4da58a594d5666e44843586d4e501efc498e39af
Yassine <qoraicheofficiel@hotmail.com>
2019-09-22 15:27:51 +0100

    update footer minor version

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-right mt-4" style="font-size: 14px;">
       <div class="text-muted">
-          <p>&copy; 2019 - <a href="https://maileclipse.io/" target="_blank">MailEclipse.io</a> v1.2</p>
+          <p>&copy; 2019 - <a href="https://maileclipse.io/" target="_blank">MailEclipse.io</a> v1.3</p>
 			<a class="github-button" href="https://github.com/qoraiche/laravel-mail-editor/issues" data-icon="octicon-issue-opened" aria-label="Issue qoraiche/laravel-mail-editor on GitHub">Issue</a>
 
 			<a class="github-button" href="https://github.com/qoraiche/laravel-mail-editor" data-icon="octicon-star" data-show-count="true" aria-label="Star qoraiche/laravel-mail-editor on GitHub">Star</a>
