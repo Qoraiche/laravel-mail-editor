3d71f0515e9b301e06ba27f8ecddb6feda6c4693
Yogendra Revanna <yogenrda.revanna@logmein.com>
2020-09-21 16:02:11 +0530

    Add assets publishing command laravel-mail-editor:install

---
+++ src/Console/VendorPublishCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace qoraiche\mailEclipse\Command;
+
+use Illuminate\Console\Command;
+
+class VendorPublishCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laravel-mail-editor:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish all MailEclipse assets from vendor packages';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('vendor:publish', [
+            "--provider" => "qoraiche\mailEclipse\mailEclipseServiceProvider"
+        ]);
+    }
+}
