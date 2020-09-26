d556cd8906c9215d7bc1c3d4b3ce710d7b873f33
ReeceM <reecestevenmay@gmail.com>
2020-09-27 00:11:57 +0200

    wip: skeletons and replacer

--- src/Templates/TemplateSkeletons.php
+++ src/Templates/TemplateSkeletons.php
@@ -3,36 +3,43 @@
 namespace Qoraiche\MailEclipse\Templates;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\View;
 use Qoraiche\MailEclipse\MailEclipse;
+use Qoraiche\MailEclipse\Utils\Replacer;
 
 class TemplateSkeletons
 {
-
     /**
      * Gets the template Skeletons from the configuration file.
      *
      * @return \Illuminate\Support\Collection
-     * @throws Illuminate\Contracts\Container\BindingResolutionException
      */
-    public static function getTemplateSkeletons()
+    public function skeletons()
     {
         return new Collection(config('maileclipse.skeletons'));
     }
 
-    public static function getTemplateSkeleton($type, $name, $skeleton)
+    /**
+     *
+     * @param mixed $type
+     * @param mixed $name
+     * @param mixed $skeleton
+     * @return array|void
+     */
+    public function templateSkelton($type, $name, $skeleton)
     {
-
         $skeletonView = MailEclipse::VIEW_NAMESPACE."::skeletons.{$type}.{$name}.{$skeleton}";
 
-        if (view()->exists($skeletonView)) {
-            $skeletonViewPath = View($skeletonView)->getPath();
+        if (View::exists($skeletonView)) {
+
+            $skeletonViewPath = View::make($skeletonView)->getPath();
             $templateContent = file_get_contents($skeletonViewPath);
 
             return [
                 'type' => $type,
                 'name' => $name,
                 'skeleton' => $skeleton,
-                'template' => self::templateComponentReplace($templateContent, true),
+                'template' => Replacer::toEditor($templateContent),
                 'view' => $skeletonView,
                 'view_path' => $skeletonViewPath,
             ];
--- src/Utils/Replacer.php
+++ src/Utils/Replacer.php
@@ -5,71 +5,71 @@ namespace Qoraiche\MailEclipse\Utils;
 class Replacer
 {
     /**
+     * The pattern for the blade syntax to replace.
+     *  eg `'/@component/i'`
+     *
+     * @var array
+     */
+    protected $bladeMatchPatterns = [];
+
+    /**
+     * The syntax that replaces the blade files
+     * eg `'[component]: # '`
+     *
+     * @var array
+     */
+    protected $bladeReplacePatterns = [];
+
+    /**
+     * The patterns of the editor to match.
+     * eg `'/\[component]:\s?#\s?/i'`
+     *
+     * @var array
+     */
+    protected $editorMatchPatterns = [];
+
+    /**
+     * The blade syntax to change to from the editor.
+     * eg `'@component'`
+     *
+     * @var array
+     */
+    protected $editorReplacePatterns = [];
+
+    /**
+     * Create a new instance of the replacer.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->generateRegex();
+    }
+
+    /**
      * Take the format of the Blade syntax and convert to one the editor understands.
      *
      * @param mixed $content
      * @return string|string[]|null
      */
-    public function editor($content)
+    public static function toEditor($content)
     {
-        $patterns = [
-            '/@component/i',
-            '/@endcomponent/i',
-            '/@yield/',
-            '/@section/',
-            '/@endsection/',
-            '/@extends/',
-            '/@parent/',
-            '/@slot/',
-            '/@endslot/',
-        ];
+        $self = new self();
 
-        $replacements = [
-            '[component]: # ',
-            '[endcomponent]: # ',
-            '[yield]: # ',
-            '[section]: # ',
-            '[endsection]: # ',
-            '[extends]: # ',
-            '[parent]: # ',
-            '[slot]: # ',
-            '[endslot]: # ',
-        ];
-
-        return $this->replace($patterns, $replacements, $content);
+        return $self->replace($self->bladeMatchPatterns, $self->bladeReplacePatterns, $content);
     }
 
     /**
      * Take the data from the editor and return a format that the Blade syntax.
      *
      * @param mixed $content
-     * @return void
+     * @return string|string[]|null
      */
-    public function fromEditor($content)
+    public static function toBlade($content): string
     {
-        $patterns = [
-            '/\[component]:\s?#\s?/i',
-            '/\[endcomponent]:\s?#\s?/i',
-            '/\[yield]:\s?#\s?/i',
-            '/\[section]:\s?#\s?/i',
-            '/\[endsection]:\s?#\s?/i',
-            '/\[extends]:\s?#\s?/i',
-            '/\[parent]:\s?#\s?/i',
-            '/\[slot]:\s?#\s?/i',
-            '/\[endslot]:\s?#\s?/i',
-        ];
+        $self = new self();
 
-        $replacements = [
-            '@component',
-            '@endcomponent',
-            '@yield',
-            '@section',
-            '@endsection',
-            '@extends',
-            '@parent',
-            '@slot',
-            '@endslot',
-        ];
+        return $self->replace($self->editorMatchPatterns, $self->editorReplacePatterns, $content);
     }
 
     /**
@@ -83,5 +83,35 @@ class Replacer
     protected function replace(array $patterns, array $replacements, $content)
     {
         return preg_replace($patterns, $replacements, $content);
+    }
+
+    /**
+     * Generate the regex patterns from a single list.
+     *
+     * This also ensures that the replace array is the same length as the match array
+     *
+     * @return void
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    protected function generateRegex()
+    {
+        $components = config('maileclipse.components');
+
+        $this->bladeMatchPatterns = array_map(function($component) {
+            return "/@${component}/i";
+        }, $components);
+
+        $this->bladeReplacePatterns = array_map(function($component) {
+            return "[${component}]: # ";
+        }, $components);
+
+
+        $this->editorMatchPatterns = array_map(function($component) {
+            return "/\[${component}]:\s?#\s?/i";
+        }, $components);
+
+        $this->editorReplacePatterns = array_map(function($component) {
+            return "@${component}";
+        }, $components);
     }
 }
